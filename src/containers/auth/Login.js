8546b1df14e2de6051f61f723fca5ef2efcb4abb
HakamRaza <ismatcd@yahoo.com>
2020-07-24 16:43:49 +0800

    customize front end login page and register page css

--- src/containers/auth/Login.js
+++ src/containers/auth/Login.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 import { connect } from "react-redux";
 import Actions from "../../actions";
-
+import "./public.css";
+import {Link} from 'react-router-dom';
 
 class Login extends Component {
   constructor(props) {
@@ -37,35 +38,48 @@ class Login extends Component {
   render() {
     console.log("DATA", this.props.getLoginData);
     return (
-      <div>
-        <h1>this is login screen</h1>
-        <input
-          type="text"
-          placeholder="email"
-          trigger function every time it change
-          onChange={email => {
-            //update at state
-            this.setState({ emailInput: email.target.value });
-          }}
+      <div className = "container">
+          <h1>Log In Here</h1>
+          <br/>
 
-          // check what is type
-          // onChange={(email) => {
-          //   console.log(email.target.value);
-          // }}
+        <div className = "card">
 
-        />
-        <input
-          type="text"
-          placeholder="password"
-          onChange={password => {
-            this.setState({ passwordInput: password.target.value });
-          }}
-        />
+          <label>Email:</label>
+          <br/>
+          <input
+            type="text"
+            placeholder="email"
+            trigger function every time it change
+            onChange={email => {
+              //update at state
+              this.setState({ emailInput: email.target.value });
+            }}
 
-        {/* <button onClick={this.onSubmitPressed}>Login</button> */}
-        {/* same as */}
-        <button onClick={() => this.onSubmitPressed()}>Login</button>
+            // check what is type
+            // onChange={(email) => {
+            //   console.log(email.target.value);
+            // }}
 
+          />
+          <br/>
+          <label>Password:</label>
+          <br/>
+          <input
+            type="text"
+            placeholder="password"
+            onChange={password => {
+              this.setState({ passwordInput: password.target.value });
+            }}
+          /><br/>
+
+          {/* <button onClick={this.onSubmitPressed}>Login</button> */}
+          {/* same as */}
+
+          <button onClick={() => this.onSubmitPressed()}>Login</button>
+
+          <Link to={"/register"}><p>Register Here</p></Link>
+
+        </div>
       </div>
     );
   }
--- src/containers/auth/register.js
+++ src/containers/auth/register.js
@@ -3,7 +3,7 @@ import React from 'react';
 //to dispatch the action from VIEW
 import {connect} from 'react-redux';
 import Actions from "../../actions";
-
+import { Link } from 'react-router-dom';
 
 class Register extends React.Component{
     constructor(props){
@@ -34,34 +34,43 @@ class Register extends React.Component{
 
     render(){
         return(
-            <div>
+            <div className="container">
+                <h1>Register Here</h1>
 
-                <h1>This is Register Page</h1>
-    
-                {/* //input form */}
-                <input type="text" placeholder="name" onChange={(name)=> this.setState({
-                    name:name.target.value
-                })}
-                />
-    
-                <input type="text" placeholder="email" onChange={(email)=> this.setState({
-                    email:email.target.value
-                })}
-                />
-    
-    
-                <input type="text" placeholder="password" onChange={(password)=> this.setState({
-                    password:password.target.value
-                })}
-                />
-    
-                <input type="text" placeholder="password_confirmation" onChange={(password_confirmation)=> this.setState({
-                    password_confirmation:password_confirmation.target.value
-                })}
-                />
-    
-                <button onClick={()=> this.onSubmitPressed()}>Submit</button>
+                <div className = "card">
+                    {/* //input form */}
 
+                    <label>Name:</label>
+                    <br/>
+                    <input type="text" placeholder="name" onChange={(name)=> this.setState({
+                        name:name.target.value
+                    })}
+                    />
+
+                    <label>Email:</label>
+                    <br/>
+                    <input type="text" placeholder="email" onChange={(email)=> this.setState({
+                        email:email.target.value
+                    })}
+                    />
+        
+                    <label>Password:</label>
+                    <br/>
+                    <input type="text" placeholder="password" onChange={(password)=> this.setState({
+                        password:password.target.value
+                    })}
+                    />
+                    <label>Retype Password:</label>
+                    <br/>
+                    <input type="text" placeholder="password_confirmation" onChange={(password_confirmation)=> this.setState({
+                        password_confirmation:password_confirmation.target.value
+                    })}
+                    />
+        
+                    <button onClick={()=> this.onSubmitPressed()}>Register</button>
+
+                    <Link to={"/login"}><p>Login</p></Link>
+                </div>
             </div>
         );
     }
