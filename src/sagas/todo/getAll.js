287c28021d4a7801f45d21b6d52685d531325f81
HakamRaza <ismatcd@yahoo.com>
2020-07-28 09:20:46 +0800

    repair token get from store

--- src/sagas/todo/getAll.js
+++ src/sagas/todo/getAll.js
@@ -3,23 +3,21 @@ import Actions from "../../actions";
 import * as api from "../../api";
 
 //import token from store
-import {getStore} from '../../store/configureStore';
+import { store } from 'store/index';
 
 
 //compile token, request to call the api
 function* getAll() {
     
-    console.log("getAll saga");
+    // console.log("getAll saga");
 
-    // let store = getStore().getState();
+    let token = store.getState().PROFILE.userSession.data;
     // let token = Actions.activateUserSession(store).data;
-    // console.log("the token is:", token);
 
-    // const headers = {Authorization:`Bearer ${token}`};
+    console.log("the token is:", token);
+
+    const headers = {Authorization:`Bearer ${token}`};
     
-    //hard coded the token
-    const headers = {Authorization:`Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTU5NTgyNzIzOSwiZXhwIjoxNTk1ODMwODM5LCJuYmYiOjE1OTU4MjcyMzksImp0aSI6Ik42VU1XRjR6ckE2d2hWaHYiLCJzdWIiOjIsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.3gh6rTzCK8R0yBG3IQtvRY5D1hWBEvLrYkPVB4v14o8`};
-
     const {response, error} = yield call (api.getAll, headers);
 
 
